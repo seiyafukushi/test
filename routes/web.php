d1e276a7a709e163dc350a8dcf417bc728242550
seiyafukushi <seiyaf294@gmail.com>
2022-11-14 16:08:28 +0900

    add 新規投稿作成

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,5 @@ Route::get('/', function() {
     return view('posts/index');});
 */
 Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class,'show']);
