431667d600995087a2fda669e4f4f4a2c8048854
seiyafukushi <seiyaf294@gmail.com>
2022-11-14 13:22:26 +0900

    表示設定

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,21 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //use宣言は外部にあるクラスをPostController内にインポートできる。
-    //この場合、App\Models内のPostクラスをインポートしている。
-    /**
-     * Post一覧を表示する
-    * 
-    * @param Post Postモデル
-    * @return array Postモデルリスト
-     */
-    public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+    public function index(Post $post)
     {
-        return $post->get();//$postの中身を戻り値にする。
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(5)]);
+    } 
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
     }
 }
+?>
